cb8794c75b82f42c707ee77666aafc5b8946252e
ace <aceyunus@gmail.com>
2023-07-07 11:02:27 +0700

    field note on db

---
+++ db/20230707.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `wh-mg`.`permintaan` ADD COLUMN `note` text NULL;
+ALTER TABLE `wh-mg`.`permintaan_pembelian` ADD COLUMN `note` text NULL;
+ALTER TABLE `wh-mg`.`pembelian` ADD COLUMN `note` text NULL;
